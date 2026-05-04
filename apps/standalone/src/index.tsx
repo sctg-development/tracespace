76f547ed30fffc14ebccaad3f7d3ccd633dd6784
Ronan <ronan.le_meillat@highcanfly.club>
2026-05-04 13:03:01 +0200

    update démo

--- apps/standalone/src/index.tsx
+++ apps/standalone/src/index.tsx
@@ -5,16 +5,18 @@ import './index.css'
 import {TracespaceViewer} from '@sctg/tracespace-view'
 
 createRoot(document.querySelector('[data-hook=root]') as Element).render(
-  <div className="h-screen">
-    <TracespaceViewer
-      showNav={true}
-      showPageTitle={false}
-      showPageTitleLogo={false}
-      pageTitle="tracespace"
-      pageSubtitle="react standalone example"
-      showLoadFiles={false}
-      useStorage={false}
-      file="/arduino-uno.zip"
-    />
+  <div  className="h-screen w-screen flex items-center justify-center">
+    <div className="h-3/4 w-3/4 rounded-lg shadow-lg overflow-hidden">
+      <TracespaceViewer
+        showNav={true}
+        showPageTitle={false}
+        showPageTitleLogo={false}
+        pageTitle="tracespace"
+        pageSubtitle="react standalone example"
+        showLoadFiles={false}
+        useStorage={false}
+        file="/arduino-uno.zip"
+      />
+    </div>
   </div>
 )
